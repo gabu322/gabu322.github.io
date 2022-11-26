af806f533fb61cabd6dd0ee0319a6c82e995086e
Gabriel DallOrto <gabrieldallorto@hotmail.com>
2022-11-26 00:53:39 -0300

    Exercise 11 unfinished

--- oneBitCode/JavaScript1/ex11/index.js
+++ oneBitCode/JavaScript1/ex11/index.js
@@ -1,7 +1,7 @@
 function addPlayer() {
-    const position = document.getElementById('position').value;
-    const name = document.getElementById('name').value;
-    const number = document.getElementById('number').value;
+    const position = document.getElementById("position").value
+    const name = document.getElementById("name").value
+    const number = document.getElementById("number").value
     
     const confirmation = confirm("Escalar " + name + " como " + position + "?")
 
